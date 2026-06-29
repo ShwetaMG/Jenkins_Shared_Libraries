5a269e1ffcf55ca2c07b3ba9232cb547d524d8ad
ShwetaMG <89741655+ShwetaMG@users.noreply.github.com>
2026-06-29 17:26:20 +0530

    Refactor push function parameter order and structure

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,12 +1,11 @@
-def call(String Project, String ImageTag, String dockerbubuser){
-  echo "pushing the image"
-               withCredentials([usernamePassword(
-                    credentialsId:"Docker-Cred",
-                    usernameVariable:"dockerHubUser", 
-                    passwordVariable:"dockerHubPass")])
-  {
-                sh " docker login -u ${dockerHubUser} -p ${dockerHubPass}"
-  }
-                sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
-    
+def call(String DockerHubUser, String Project, String ImageTag){
+    echo "pushing the image"
+    withCredentials([usernamePassword(
+        credentialsId: "Docker-Cred",
+        usernameVariable: "dockerHubUser",
+        passwordVariable: "dockerHubPass")]) 
+    {
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+        sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+    }
 }
