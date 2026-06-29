8a851da37533d5b70dac70569cd2316ca064640d
ShwetaMG <89741655+ShwetaMG@users.noreply.github.com>
2026-06-29 16:54:48 +0530

    Fix branch variable in clone function

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,5 @@
 def call(String url, Srtring branch){
    echo "cloning the repo code" 
-   git url: "${url}", branch:"${main}"
+   git url: "${url}", branch:"${branch}"
     echo "code cloning successfull!"
 }
