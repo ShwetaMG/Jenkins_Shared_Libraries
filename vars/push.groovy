096d94437a490670daca4c892b48d552d02fa08b
ShwetaMG <89741655+ShwetaMG@users.noreply.github.com>
2026-06-29 16:39:20 +0530

    Create push.groovy for Docker image pushing
    Add a function to push Docker images to Docker Hub.

---
+++ vars/push.groovy
@@ -0,0 +1,12 @@
+def call(String Project, String ImageTag, String Dockerbubuser){
+  echo "pushing the image"
+               withCredentials([usernamePassword(
+                    credentialsId:"Docker-Cred",
+                    usernameVariable:"dockerHubUser", 
+                    passwordVariable:"dockerHubPass")])
+  {
+                sh " docker login -u $(dockerHubUser} -p $(dockerHubPass)"
+  }
+                sh "docker push ${dockerHubUser}/$(Project):$(ImageTagName)"
+    
+}
