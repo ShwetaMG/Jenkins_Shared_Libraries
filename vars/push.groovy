10a34598d218da1de6e2b1cc0c502e23b21fc52c
ShwetaMG <89741655+ShwetaMG@users.noreply.github.com>
2026-06-29 17:05:25 +0530

    Fix syntax error in Docker login command

--- vars/push.groovy
+++ vars/push.groovy
@@ -5,7 +5,7 @@ def call(String Project, String ImageTag, String Dockerbubuser){
                     usernameVariable:"dockerHubUser", 
                     passwordVariable:"dockerHubPass")])
   {
-                sh " docker login -u $(dockerHubUser} -p $(dockerHubPass)"
+                sh " docker login -u ${dockerHubUser} -p ${dockerHubPass}"
   }
                 sh "docker push ${dockerHubUser}/$(Project):$(ImageTag)"
     
