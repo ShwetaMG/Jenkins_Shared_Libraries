a5d55944eaae4e51d89abc2cdbd0dc90add22c53
ShwetaMG <89741655+ShwetaMG@users.noreply.github.com>
2026-06-29 16:55:35 +0530

    Fix typo in parameter type for clone function

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def call(String url, Srtring branch){
+def call(String url, String branch){
    echo "cloning the repo code" 
    git url: "${url}", branch:"${branch}"
     echo "code cloning successfull!"
